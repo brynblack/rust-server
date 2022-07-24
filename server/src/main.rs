826f066e576e5b8d40ab5f0dbd76e3700f16d709
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2022-07-24 23:42:22 +1000

    Added address argument

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use std::{io::Read, net::TcpListener};
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8192")?;
+    let listener = TcpListener::bind("0.0.0.0:8192")?;
     let (mut stream, _) = listener.accept()?;
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer)?;
